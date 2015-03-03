3d652b3a2e20a62bb129350044e11da03bf17eb8
marea-cobb <marea.cobb@email.wsu.edu>
2015-03-03 10:30:41 -0800

    Changed to account for patient id.

--- CobbMareaHW3.R
+++ CobbMareaHW3.R
@@ -4,12 +4,11 @@ library(Biobase)
 library(limma)
 library(beadarray)
 library(pheatmap)
-
 #' Creates the R markdown files.
-library("knitr")
-opts_knit$set(progress = FALSE, verbose = FALSE, message=FALSE)
-spin(hair = "CobbMareaHW3.R", format = "Rmd")
-file.rename("CobbMareaHW3.md", "CobbMareaHW3.Rmd")
+# library("knitr")
+# opts_knit$set(progress = FALSE, verbose = FALSE, message=FALSE)
+# spin(hair = "CobbMareaHW3.R", format = "Rmd")
+# file.rename("CobbMareaHW3.md", "CobbMareaHW3.Rmd")
 
 #Loads GEO libraries and specific dataset
 library(GEOmetadb)
@@ -35,47 +34,42 @@ gset <- gse[[idx]]
 gset_new <- gset
 
 sanitize_pdata <- function(pd) {
-  keepCols <- c("title", "geo_accession", "status", "submission_date", "last_update_date", "type", 
-                "channel_count", "source_name_ch1", "organism_ch1", "molecule_ch1", "extract_protocol_ch1",
-                "label_ch1", "label_protocol_ch1", "taxid_ch1", "hyb_protocol", "scan_protocol", "description",
-                "data_processing", "platform_id", "contact_name", "contact_email", "contact_department",
-                "contact_institute", "contact_address", "contact_city", "contact_state", "contact_zip/postal_code",
-                "contact_country", "supplementary_file", "data_row_count", "characteristics_ch1", "characteristics_ch1.1", "characteristics_ch1.2")
+  keepCols <- c("title", "characteristics_ch1", "characteristics_ch1.1", "characteristics_ch1.2")
   pd <- pd[, keepCols]
-  pd$x <- NULL
+  
+  pd$title <- sapply(strsplit(as.character(pd$title), "_"), "[", 2)
   pd$characteristics_ch1 <- gsub(".*: ", "", pd$characteristics_ch1)
   pd$characteristics_ch1.1 <- gsub(".*:", "", pd$characteristics_ch1.1)
   pd$characteristics_ch1.2 <- gsub(".*: ", "", pd$characteristics_ch1.2)
+  pd <- pd[,c(1,2,3,4)]
+  names(pd) <- c("ptid", "HCV", "celltype", "treatment")
   pd
 }
 
 pData(gset_new) <- sanitize_pdata(pData(gset_new))
 
-macrophages <- gset_new[, grepl("Monocyte-derived Macrophage", pData(gset_new)$characteristics_ch1.1)]
-pbmcs <- gset_new[, grepl("PBMC", pData(gset_new)$characteristics_ch1.1)]
+macrophages <- gset_new[, grepl("Monocyte-derived Macrophage", pData(gset_new)$celltype)]
+pbmcs <- gset_new[, grepl("PBMC", pData(gset_new)$celltype)]
 
-# macrophages$characteristics_ch1.2
-
-mm_macrophages <- model.matrix(~characteristics_ch1.2, macrophages) # design matrix
+mm_macrophages <- model.matrix(~treatment+ptid, macrophages) # design matrix
 fit_macrophages <- lmFit(macrophages, mm_macrophages) #Fit linear model for each gene given a series of arrays
 ebay_macrophages <- eBayes(fit_macrophages) # compute moderated t-statistics, moderated F-statistic, and 29/
 colnames(fit_macrophages$coef)
 
-topTreatment_macrophages <- topTable(ebay_macrophages, coef = "characteristics_ch1.2Poly IC H", number = Inf,
-                  sort.by = "none")
+topTreatment_macrophages <- topTable(ebay_macrophages, coef = "treatmentPoly IC H", number = Inf,
+                  p.value=0.05, lfc=log2(1.5), sort.by = "p")
 
 selected_macrophages  <- p.adjust(ebay_macrophages$p.value[, 2]) <0.1
 esetSel_macrophages <- macrophages [selected_macrophages, ]
 heatmap(exprs(esetSel_macrophages))
 
-
 mm_pbmcs <- model.matrix(~characteristics_ch1.2, pbmcs) # design matrix
 fit_pbmcs <- lmFit(pbmcs, mm_pbmcs) #Fit linear model for each gene given a series of arrays
 ebay_pbmcs <- eBayes(fit_pbmcs) # compute moderated t-statistics, moderated F-statistic, and 29/
 
-topTreatmentPBMCs <- topTable(ebay_pbmcs, coef = "characteristics_ch1.2Poly IC L", number = Inf,
-                         sort.by = "none")
+topTreatmentPBMCs <- topTable(ebay_pbmcs, coef = "treatmentPoly IC L", number = Inf,
+                         p.value=0.05, lfc=log2(1.5), sort.by = "p")
 
 selected_pbmcs  <- p.adjust(ebay_pbmcs$p.value[, 2]) <0.1
 esetSel <- pbmcs [selected_pbmcs, ]
-heatmap(exprs(esetSel))
+heatmap(topTreatmentPBM)
